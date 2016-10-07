0551ad22104bcf7f4aee15079fb47a1a9dce3584
Squidly271 <azawadzki@arrowgames.com>
2016-10-06 23:28:17 -0400

    thinks its ready

--- source/ransomware.bait/usr/local/emhttp/plugins/ransomware.bait/include/exec.php
+++ source/ransomware.bait/usr/local/emhttp/plugins/ransomware.bait/include/exec.php
@@ -12,7 +12,7 @@ function getSettings() {
   }
   return $settings;
 }
-echo "ok";
+
 switch ($_POST['action']) {
   case 'applySettings':
     $settings = getSettings();
@@ -30,11 +30,21 @@ switch ($_POST['action']) {
     if ( ! is_file($ransomwarePaths['smbShares']) ) { break; }
     logger("Resetting SMB permissions to normal per user selection");
     rename($ransomwarePaths['smbShares'],"/etc/samba/smb-shares.conf");
+    exec("rm -rf /boot/config/shares");
+    exec("mkdir -p /boot/config/shares");
+    exec("cp /boot/config/plugins/ransomware.bait/shareBackup/* /boot/config/shares");
     @unlink($ransomwarePaths['detected']); # also kill the event
     exec("/etc/rc.d/rc.samba restart");
     break;
   case 'setReadOnly':
     smbReadOnly();
+    break;
+  case 'getStatus':
+    if ( is_file($ransomwarePaths['PID']) ) {
+      echo "<font color='green'>Running</font>";
+    } else {
+      echo "<font color='red'>Not Running</font>";
+    }
     break;
 }
 ?>
