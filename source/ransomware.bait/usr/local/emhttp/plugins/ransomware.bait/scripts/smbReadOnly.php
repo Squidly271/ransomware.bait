1476c1bedf643a4f21d63a4d6785dfa58ed7a64f
Squidly271 <azawadzki@arrowgames.com>
2016-11-11 21:41:53 -0500

    Prevent share backups being overwritten

--- source/ransomware.bait/usr/local/emhttp/plugins/ransomware.bait/scripts/smbReadOnly.php
+++ source/ransomware.bait/usr/local/emhttp/plugins/ransomware.bait/scripts/smbReadOnly.php
@@ -12,6 +12,10 @@ require_once("/usr/local/emhttp/plugins/ransomware.bait/include/paths.php");
 if ( isfile($ransomwarePaths['smbReadOnlyProcess']) ) {
   exit;
 }
+if ( isdir("/boot/config/plugins/ransomware.bait/shareBackup") ) {
+  logger("Double attack detected.  Possible misconfigured settings allowing a share (downloads?) to be deleted locally");
+  exit;
+}
 $settings = readSettingsFile();
 smbReadOnly($settings);
 
