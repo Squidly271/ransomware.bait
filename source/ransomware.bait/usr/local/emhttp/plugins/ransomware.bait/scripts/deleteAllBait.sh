d1bb0a2ac94fe88e7ac1a115eba835b3119865a8
Squidly271 <azawadzki@arrowgames.com>
2016-11-27 01:30:09 -0500

    fdasf

--- source/ransomware.bait/usr/local/emhttp/plugins/ransomware.bait/scripts/deleteAllBait.sh
+++ source/ransomware.bait/usr/local/emhttp/plugins/ransomware.bait/scripts/deleteAllBait.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/local/emhttp/plugins/ransomware.bait/scripts/deleteBait.php
-/usr/local/emhttp/plugins/ransomware.bait/scripts/deleteBaitShare.php
+/usr/local/emhttp/plugins/ransomware.bait/scripts/deleteBait.php > /dev/null 2>&1
+/usr/local/emhttp/plugins/ransomware.bait/scripts/deleteBaitShare.php > /dev/null 2>&1
 
